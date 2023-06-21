cd6fe6bdc355f5ec7de547ed30a5d19f24f448ab
Natali <saromydova-natasha@yandex.ru>
2023-06-21 20:56:48 +0300

    Homework1 C# Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int num = ReadInt("Введите число больше нуля: ");
+
+int count = 1;
+while (count <= num)
+{
+    Console.WriteLine(count * count * count);
+    count++;
+}
